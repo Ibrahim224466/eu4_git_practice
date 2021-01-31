e00d232969662fc06efa4e45f21360f29fde01ab
Ibrahim224466 <mercanibrahim@hotmail.com>
2021-01-31 01:36:37 +0100

    this 3rd time

--- src/main/java/Trying.java
+++ src/main/java/Trying.java
@@ -2,7 +2,8 @@ public class Trying {
 
     public static void main(String[] args) {
         System.out.println("This is second trying.");
-        //try what you seeqqqq
+        //try what you seeqqqq ahah
+        //dfjdjdj
     }
 
 
