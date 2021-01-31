ff48ee562003c86423423b18f4f6993091f67ca7
Ibrahim224466 <mercanibrahim@hotmail.com>
2021-01-31 19:10:08 +0100

    Sunday IntelliJ commit

--- src/main/java/Trying.java
+++ src/main/java/Trying.java
@@ -4,6 +4,8 @@ public class Trying {
         System.out.println("This is second trying.");
         //try what you seeqqqq ahah
         //dfjdjdj
+
+        System.out.println("This is my change Sunday from IntelliJ");
     }
 
 
