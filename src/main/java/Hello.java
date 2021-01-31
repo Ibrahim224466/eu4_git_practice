6a3c25d9e2f1190ef835e0f2a72a3768d93a19eb
Ibrahim224466 <mercanibrahim@hotmail.com>
2021-01-31 01:02:12 +0100

    this second time

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,8 +4,7 @@ public class Hello {
         System.out.println("Hello World");
         System.out.println("EU4 is doing great!");
         //forgot to change commit message
-        //trying
-
+        //trying rrrrr
 
     }
 
---
+++ src/main/java/Trying.java
@@ -0,0 +1,8 @@
+public class Trying {
+
+    public static void main(String[] args) {
+        System.out.println("This is second trying.");
+    }
+
+
+}
